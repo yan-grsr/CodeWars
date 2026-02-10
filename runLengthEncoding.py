2a5f19da18a3b77ec7a8cff8105de5a6ee2c1313
yan-grsr <yan.garrissere@gmail.com>
2026-02-10 17:17:25 +0100

    Run Length Encoding

---
+++ runLengthEncoding.py
@@ -0,0 +1,21 @@
+def run_length_encoding(s):
+    rle_list = []
+    j = 0
+    if s == '':
+        return rle_list
+    
+    rle_list.append([1,s[0]])
+
+    for i in range(1,len(s)):
+        if s[i] == s[i-1]:
+            rle_list[j][0] += 1
+        else:
+            j += 1
+            rle_list.append([1,s[i]])
+
+    return rle_list
+
+if __name__ == '__main__':
+    print(run_length_encoding(''))
+    print(run_length_encoding('abc'))
+    print(run_length_encoding('aaaabbbccd'))
